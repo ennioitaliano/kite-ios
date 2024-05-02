ee7ccd2c113851980ff3983dbec19636f3ce35a1
Ennio Italiano <e.italiano@zero12.it>
2024-05-02 18:47:02 +0200

    Initial Commit

---
+++ Air Quality Charts/Air_Quality_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_Quality_ChartsApp.swift
+//  Air Quality Charts
+//
+//  Created by Ennio Italiano on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Air_Quality_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
