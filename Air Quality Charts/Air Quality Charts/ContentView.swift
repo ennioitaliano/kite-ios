9c955d35e83ee35d95e42eaa95c28931444bab0e
Ennio Italiano <ennio.italiano@gmail.com>
2024-05-12 17:54:14 +0200

    View to test API calls

--- Air Quality Charts/Air Quality Charts/ContentView.swift
+++ Air Quality Charts/Air Quality Charts/ContentView.swift
@@ -5,15 +5,53 @@
 //  Created by Ennio Italiano on 09/05/24.
 //
 
+import Dependencies
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @Dependency(\.airPollutionRemoteRepository) private var APRemoteRepository
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Current") {
+                Task {
+                    print(
+                        try await APRemoteRepository.getCurrent(
+                            for: CLLocationCoordinate2D(
+                                latitude: 50,
+                                longitude: 50
+                            )
+                        )
+                    )
+                }
+            }
+            Button("Forecast") {
+                Task {
+                    print(
+                        try await APRemoteRepository.getForecast(
+                            for: CLLocationCoordinate2D(
+                                latitude: 50,
+                                longitude: 50
+                            )
+                        )
+                    )
+                }
+            }
+            Button("Historical") {
+                Task {
+                    print (
+                        try await APRemoteRepository.getHistorical(
+                            for: CLLocationCoordinate2D(
+                                latitude: 50,
+                                longitude: 50
+                            ),
+                            start: .now,
+                            end: .now
+                        )
+                    )
+                }
+            }
         }
         .padding()
     }
