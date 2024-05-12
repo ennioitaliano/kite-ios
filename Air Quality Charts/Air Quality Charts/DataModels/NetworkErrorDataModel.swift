e5f7fd656df2334d72085e66864322e39c1b9e24
Ennio Italiano <ennio.italiano@gmail.com>
2024-05-12 17:50:36 +0200

    Added NetworkErrorDataModel

---
+++ Air Quality Charts/Air Quality Charts/DataModels/NetworkErrorDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkErrorDataModel.swift
+//  Air Quality Charts
+//
+//  Created by Ennio Italiano on 12/05/24.
+//
+
+import Foundation
+
+struct NetworkErrorDataModel: Error, Decodable, Equatable {
+    let detail: String?
+
+    enum CodingKeys: String, CodingKey {
+        case detail
+     }
+}
