4c70d47fe5c6745dba53395f32f92dfebc549aad
Ennio Italiano <ennio.italiano@gmail.com>
2024-05-12 17:53:32 +0200

    Created logs categories and dependencies

---
+++ Air Quality Charts/Air Quality Charts/Utils/Logs/LogsCategories.swift
@@ -0,0 +1,16 @@
+//
+//  LogsCategories.swift
+//  Air Quality Charts
+//
+//  Created by Ennio Italiano on 12/05/24.
+//
+
+import Foundation
+
+enum LogsCategories: String {
+    case general
+    case network
+    case localRepository
+    case realm
+    case useCase
+}
---
+++ Air Quality Charts/Air Quality Charts/Utils/Logs/LogsDependency.swift
@@ -0,0 +1,23 @@
+//
+//  LogsDependency.swift
+//  Air Quality Charts
+//
+//  Created by Ennio Italiano on 12/05/24.
+//
+
+import CommonLogging
+import Dependencies
+import Foundation
+
+enum LogsDependencyKey: DependencyKey {
+    static var liveValue: CommonLogging<LogsCategories> {
+        return CommonLogging<LogsCategories>()
+    }
+}
+
+extension DependencyValues {
+    var logger: CommonLogging<LogsCategories> {
+        get { self[LogsDependencyKey.self] }
+        set { self[LogsDependencyKey.self] = newValue }
+    }
+}
