d77b015c78bb19c4c7b724728ba2bdf003552a3c
Ennio Italiano <ennio.italiano@gmail.com>
2024-05-12 17:50:47 +0200

    Created environment and dependencies

---
+++ Air Quality Charts/Air Quality Charts/Environment/Environment.swift
@@ -0,0 +1,23 @@
+//
+//  Environment.swift
+//  Air Quality Charts
+//
+//  Created by Ennio Italiano on 12/05/24.
+//
+
+import Foundation
+
+protocol EnvironmentAPI {
+    var baseURL: URL { get }
+    var APIKey: String { get }
+}
+
+struct Environment: EnvironmentAPI {
+    let baseURLPath: String = "https://api.openweathermap.org"
+    var baseURL: URL {
+        URL(string: baseURLPath)!
+    }
+    var APIKey: String {
+        "d01572226d0256cb3b47169739fff002"
+    }
+}
---
+++ Air Quality Charts/Air Quality Charts/Environment/EnvironmentDependency.swift
@@ -0,0 +1,21 @@
+//
+//  EnvironmentDependency.swift
+//  Air Quality Charts
+//
+//  Created by Ennio Italiano on 12/05/24.
+//
+
+import Dependencies
+import Foundation
+
+enum EnvironmentDependencyKeys: DependencyKey {
+    static var liveValue: EnvironmentAPI {
+        Environment()
+    }
+}
+extension DependencyValues {
+    var environment: EnvironmentAPI {
+        get { self[EnvironmentDependencyKeys.self] }
+        set { self[EnvironmentDependencyKeys.self] = newValue }
+    }
+}
